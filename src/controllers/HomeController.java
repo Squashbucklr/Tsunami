6493afaf59d06f945f745575bdefeaa5d2c03a9d
Squashbucklr <jbwar22@gmail.com>
2017-05-20 18:02:33 -0400

    twig file

--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -3,6 +3,7 @@ package controllers;
 import spark.*;
 
 public class HomeController implements Route{
+	String input;
 	@Override
 	public Object handle(Request arg0, Response arg1) throws Exception {
 		// TODO Auto-generated method stub
