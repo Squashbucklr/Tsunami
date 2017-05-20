ffd3bceb4b513097d9411353dfbfc04ffeeab2ea
Squashbucklr <jbwar22@gmail.com>
2017-05-20 19:56:16 -0400

    file upload

--- src/main/Tsunami.java
+++ src/main/Tsunami.java
@@ -1,8 +1,7 @@
 package main;
 import static spark.Spark.*;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import javax.servlet.MultipartConfigElement;
 
@@ -12,15 +11,44 @@ public class Tsunami {
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		System.out.println(13435 ^ 17389);
 		get("/", new HomeController());
 		
 		post("/", (request, response) -> {
+			OutputStream outputstream;
 		    request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
 		    try (InputStream is = request.raw().getPart("uploaded_file").getInputStream()) {
 		        // Use the input stream to create a file
+		    	File f = new File("src/uploads/idk.test");
+		    	f.createNewFile();
+		    	outputstream = new FileOutputStream(f);
+		    	int read = 0;
+		    	byte[] bytes = new byte[1024];
+		    	while ((read = is.read(bytes)) != -1){
+		    		outputstream.write(bytes, 0, read);
+		    	}
+		    	System.out.println("done!");
+		    	is.close();
+		    	outputstream.close();
 		    	
-		    }
+		    } catch (IOException e){
+		    	e.printStackTrace();
+		    } 
+//		    finally {
+//		    	if(is != null){
+//		    		try{
+//		    			is.close();
+//		    		} catch (IOException e){
+//		    			e.printStackTrace();
+//		    		}
+//		    	}
+//		    	if(outputstream != null){
+//		    		try{
+//		    			outputstream.close();
+//		    		}catch (IOException e){
+//		    			e.printStackTrace();
+//		    		}
+//		    	}
+//		    }
 		    return "File uploaded";
 		});
 	}
