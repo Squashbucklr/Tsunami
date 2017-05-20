bccac11b5da4d0334a27849eb3aa0b50303b62e6
Squashbucklr <jbwar22@gmail.com>
2017-05-20 14:35:29 -0400

    add spark and jdbc

--- src/main/Tsunami.java
+++ src/main/Tsunami.java
@@ -1,10 +1,10 @@
 package main;
-
+import static spark.Spark.*;
 public class Tsunami {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		get("/hello", (req, res) -> "Hello World");
 	}
 
 }
